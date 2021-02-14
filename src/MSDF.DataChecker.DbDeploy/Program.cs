908b7173089e824b150bd7290b468e1e3b7f5269
Jim McKay <james.mckay@instructure.com>
2021-02-14 17:15:09 -0600

    updated solution file, added the scripts as embedded resources

--- src/MSDF.DataChecker.DbDeploy/Program.cs
+++ src/MSDF.DataChecker.DbDeploy/Program.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System;
 
 namespace MSDF.DataChecker.DbDeploy
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
